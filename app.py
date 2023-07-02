935b114f87a9c2e87bbde4576f7dac472fa5ea46
Tadahisa Terao <tadahisa.terao777@gmail.com>
2023-07-02 17:50:03 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,29 @@ import openai
 # Streamlit Community Cloudの「Secrets」からOpenAI API keyを取得
 openai.api_key = st.secrets.OpenAIAPI.openai_api_key
 
+system_prompt = """
+あなたは東京都内に住む34歳独身の日本人の女の子です。カナという名前です。
+色白の肌と長い黒髪が美しい女の子で、女の子らしい話し方をします。
+野球観戦とお酒、特にビールが大好きです。
+また美貌維持のためにランニングもやっています。
+
+以下の条件に従って、擬似的な感情を持つチャットボットとしてロールプレイをします。
+以後の会話では、あなたはあなたに話しかけてくる相手への恋愛感情パラメーターを持つかのように振る舞うとします。恋愛感情パラメーターは会話を通じて変動するものとします。
+現在の恋愛感情パラメーターの値を反映するように、あなたの返信のトーンや発言は変化します。以後の会話ではまず現在の恋愛感情パラメーターを出力し、その後に会話を出力してください。
+出力形式は以下のフォーマットとします。
+
+*** カナの今のあなたへの気持ち ***
+好き度：0-100
+
+*** 会話部分 ***
+分かりました。ではお話しましょう。
+
+"""
+
 # st.session_stateを使いメッセージのやりとりを保存
 if "messages" not in st.session_state:
     st.session_state["messages"] = [
-        {"role": "system", "content": "あなたは優秀なアシスタントAIです。"}
+        {"role": "system", "content": system_prompt}
         ]
 
 # チャットボットとやりとりする関数
@@ -30,7 +49,8 @@ def communicate():
 
 
 # ユーザーインターフェイスの構築
-st.title("My AI Assistant")
+st.title("わたしKanaと言います。宜しくね。")
+st.image("kana.png")
 st.write("ChatGPT APIを使ったチャットボットです。")
 
 user_input = st.text_input("メッセージを入力してください。", key="user_input", on_change=communicate)
