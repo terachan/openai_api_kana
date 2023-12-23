601acb51c1f3b133f557d72be7d81a4717dd5031
Tadahisa Terao <tadahisa.terao777@gmail.com>
2023-12-23 19:21:30 +0900

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,8 @@ def communicate():
     user_message = {"role": "user", "content": st.session_state["user_input"]}
     messages.append(user_message)
 
-    response = openai.ChatCompletion.create(
+    #response = openai.ChatCompletion.create(
+    response = openai.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=messages
     )  
